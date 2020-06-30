eb25a7c1102c16d47601a5664df3d5512a735cbb
imagineapril <64987202+imagineapril@users.noreply.github.com>
2020-06-30 23:54:16 +0300

    Add files via upload

---
+++ Task03.java
@@ -0,0 +1,19 @@
+package ru.timokhina.lesson12.task03;
+
+public class Task03 {
+    public static void main(String[] args) throws Exception {
+         {
+             int a;
+             int b;
+             int dev;
+             a = 10;
+             b = 0;
+            try {
+                dev = a / b;
+            }
+            catch (Exception ex)    {
+            System.out.println(ex.getMessage());}
+         }
+    }
+}
+
